9f3b54b95019309f7cacc95f36bdd461111f4257
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-28 18:16:21 -0800

    broken map

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
+import { useState } from 'react';
 import './App.css'; /* Global CSS */
 import styles from './App.module.css'; /* CSS Modules */
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 
 export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 45.5472,
+    longitude: -122.6417,
+    width: '100vw',
+    height: '100vh',
+    zoom: 14,
+  });
+
+  console.log('viewport', viewport);
+
   return (
-    <h1
-      className={`
-      bg-green-400
-        text-3xl
-        text-center
-      text-white
-        font-bold
-        p-10
-        w-1/2
-        mx-auto
-        mt-10
-        ${styles.myCustomCssClass}
-      `}
-    >
-      Hello, World!
-    </h1>
+    <div className={styles.map}>
+      <ReactMapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={process.env.MapboxAccessToken}
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        <div className={styles.nav}>
+          <NavigationControl />
+        </div>
+      </ReactMapGL>
+    </div>
   );
 }
