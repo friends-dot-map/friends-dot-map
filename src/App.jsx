a61932e06a7c787b3a4c7bb79ade86f764ff0bb8
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-28 22:40:53 -0800

    map working as intended, opt-in user location tracking is functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import './App.css'; /* Global CSS */
 import styles from './App.module.css'; /* CSS Modules */
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import ReactMapGL, {
+  GeolocateControl,
+  Marker,
+  NavigationControl,
+} from 'react-map-gl';
 
 export default function App() {
   const [viewport, setViewport] = useState({
@@ -12,20 +16,21 @@ export default function App() {
     zoom: 14,
   });
 
-  console.log('viewport', viewport);
-
   return (
     <div className={styles.map}>
       <ReactMapGL
         {...viewport}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        mapboxApiAccessToken={process.env.MapboxAccessToken}
-        onViewportChange={(viewport) => {
+        reuseMaps
+        style={{ position: 'relative', width: '100%', height: '100%' }}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        mapboxAccessToken={process.env.MapboxAccessToken}
+        onMove={(viewport) => {
           setViewport(viewport);
         }}
       >
         <div className={styles.nav}>
           <NavigationControl />
+          <GeolocateControl trackUserLocation showUserHeading />
         </div>
       </ReactMapGL>
     </div>
