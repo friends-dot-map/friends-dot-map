bb258a18b60741217c2ecf6ef55f56d77607c5cd
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-03-01 14:55:59 -0800

    login page and functionality working yay

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function AuthForm() {
-  return <div>AuthForm</div>;
+export default function AuthForm({ handleAuth, isSigningUp }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAuth(email, password);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="please enter your email"
+          />
+        </label>
+        <label>
+          password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="please enter your password"
+          />
+        </label>
+        <button onClick={handleSubmit}>
+          {isSigningUp ? 'Register' : 'Sign In'}
+        </button>
+      </form>
+    </div>
+  );
 }
