fe624616f9d94a29648e027f1d1c7361ade86847
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-03-02 12:19:52 -0800

    wrote coords function for supabase, cprrectly getting and setting coords now

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import Map from '../../components/Map/Map';
+import { useUser } from '../../context/userContext';
+import { updateCoords } from '../../services/profiles';
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
+  const [coords, setCoords] = useState({});
   const [viewport, setViewport] = useState({
     latitude: '',
     longitude: '',
     zoom: 14,
   });
+  const { user } = useUser();
+
+  const [userCoords, setUserCoords] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
     const fetchLocation = () => {
       navigator.geolocation.getCurrentPosition((position) => {
         if (position && position.coords) {
           const { latitude, longitude } = position.coords;
+          setCoords({ latitude, longitude });
           setViewport((prevState) => ({ ...prevState, latitude, longitude }));
           setLoading(false);
         }
@@ -22,24 +31,32 @@ export default function Home() {
     fetchLocation();
   }, []);
 
-  const [userCoords, setUserCoords] = useState(null);
-  const [showPopup, setShowPopup] = useState(false);
-  const [selectedUser, setSelectedUser] = useState(null);
+  const handleCoords = async () => {
+    try {
+      await updateCoords(coords, user.id);
+      console.log(coords);
+    } catch (error) {
+      throw new Error('it isnt working..... yet');
+    }
+  };
 
   if (loading) return <h1>loading...</h1>;
 
   return (
-    <Map
-      {...{
-        viewport,
-        setViewport,
-        userCoords,
-        setUserCoords,
-        showPopup,
-        setShowPopup,
-        selectedUser,
-        setSelectedUser,
-      }}
-    />
+    <>
+      <Map
+        {...{
+          viewport,
+          setViewport,
+          userCoords,
+          setUserCoords,
+          showPopup,
+          setShowPopup,
+          selectedUser,
+          setSelectedUser,
+          handleCoords,
+        }}
+      />
+    </>
   );
 }
